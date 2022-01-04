66bbbc66d96f6bd468e213120bb087376413e283
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-01-03 20:09:14 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(){
+  cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+  cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+}
+
+const newcat = ['Broom'];
+
+function appendCat(){
+  let newcat = ([...cats, 'Broom']);
+  return newcat;
+}
+
+const firstcat = ['Arnold'];
+
+function prependCat(){
+  let firstcat = (['Arnold', ...cats]);
+  return firstcat;
+}
+
+function removeLastCat(){
+  return cats.slice(0,-1);
+}
+
+function removeFirstCat(){
+  return cats.slice(1);
+}
+  
